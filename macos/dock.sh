86be601e3018d4d89f380abfd7769523126668ce
insertYourNameHere <insertYourEmailHere>
2025-01-29 17:55:02 -0700

    Revised the dock to be what I need, and added Arc Browser

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/System/Applications/Mail.app"
-dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Anytype.app"
+dockutil --no-restart --add "/System/Applications/Messages.app"
+dockutil --no-restart --add "/System/Applications/Microsoft Excel.app"
+dockutil --no-restart --add "/System/Applications/Microsoft Outlook.app"
+dockutil --no-restart --add "/System/Applications/Obsidian.app"
+dockutil --no-restart --add "/System/Applications/Slack.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
